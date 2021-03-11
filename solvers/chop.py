e727b03fd5eb97b30d06dfddf0218b0996b7a114
Geoffrey Négiar <geoffrey_negiar@berkeley.edu>
2021-03-11 02:08:41 -0800

    Refactor

--- solvers/chop.py
+++ solvers/chop.py
@@ -58,99 +58,96 @@ class Solver(BaseSolver):
     def set_objective(self, X, y, lmbd):
         self.lmbd = lmbd
 
-        if self.device == 'cpu':
-            os.environ["CUDA_VISIBLE_DEVICES"] = ""
-        elif 'CUDA_VISIBLE_DEVICES' in os.environ:
-            del os.environ["CUDA_VISIBLE_DEVICES"]
-
         device = torch.device(self.device)
         self.X = torch.tensor(X).to(device)
         self.y = torch.tensor(y > 0, dtype=torch.float64).to(device)
 
+        _, n_features = X.shape
+
+        self.x0 = torch.zeros(n_features, dtype=X.dtype, device=X.device)
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+
+    def run_stochastic(self, n_iter):
+        # prepare dataset
+        dataset = TensorDataset(self.X, self.y)
+        loader = DataLoader(dataset, batch_size=self.batch_size)
+
+        # prepare opt variable
+        x = self.x0.clone().detach().flatten()
+        x.requires_grad_(True)
+
+        if self.solver == 'pgd':
+            optimizer = chop.stochastic.PGD([x], lr=.05, momentum=self.momentum,
+                                            normalization=self.normalization)
+
+        else:
+            raise NotImplementedError
+
+        # Optimization loop
+        counter = 0
+
+        alpha = self.lmbd / X.size(0)
+
+        while counter < n_iter:
+
+            for data, target in loader:
+                counter += 1
+
+                optimizer.zero_grad()
+                pred = data @ x
+                loss = self.criterion(pred, target)
+                loss += .5 * alpha * (x ** 2).sum()
+                loss.backward()
+                optimizer.step()
+
+        self.beta = x.detach().clone()
+
+    def run_full_batch(self, n_iter):
+        # Set up the problem
+
+        # chop's full batch optimizers require
+        # (batch_size, *shape) shape
+        x0 = self.x0.reshape(1, -1)
+
+        @chop.utils.closure
+        def logloss(x):
+
+            alpha = self.lmbd / X.size(0)
+            out = chop.utils.bmv(X, x)
+            loss = self.criterion(out, y)
+            reg = .5 * alpha * (x ** 2).sum()
+            return loss + reg
+
+        # Solve the problem
+        if self.solver == 'pgd':
+            if self.line_search:
+                step = 'backtracking'
+            else:
+                # estimate the step using backtracking line search once
+                step = None
+
+            result = chop.optim.minimize_pgd(logloss, x0,
+                                             prox=lambda x, s=None: x,
+                                             step=step,
+                                             max_iter=n_iter)
+
+        else:
+            raise NotImplementedError
+
+        self.beta = result.x
+
     def run(self, n_iter):
-        X, y, solver = self.X, self.y, self.solver
 
-        n_points, n_features = X.shape
-
-        x0 = torch.zeros(n_features, dtype=X.dtype, device=X.device)
         if n_iter == 0:
-            self.beta = x0
+            self.beta = self.x0
             return
-
-        criterion = torch.nn.BCEWithLogitsLoss()
 
         warnings.filterwarnings('ignore', category=RuntimeWarning)
 
         if self.stochastic:
-
-            # prepare dataset
-            dataset = TensorDataset(X, y)
-            loader = DataLoader(dataset, batch_size=self.batch_size)
-
-            # prepare opt variable
-            x = x0.clone().detach().flatten()
-            x.requires_grad_(True)
-
-            if solver == 'pgd':
-                optimizer = chop.stochastic.PGD([x], lr=.05, momentum=self.momentum,
-                                                normalization=self.normalization)
-
-            else:
-                raise NotImplementedError
-
-            # Optimization loop
-            counter = 0
-
-            alpha = self.lmbd / X.size(0)
-
-            while counter < n_iter:
-
-                for data, target in loader:
-                    counter += 1
-
-                    optimizer.zero_grad()
-                    pred = data @ x
-                    loss = criterion(pred, target)
-                    loss += .5 * alpha * (x ** 2).sum()
-                    loss.backward()
-                    optimizer.step()
-
-            self.beta = x.detach().clone()
-
-        # Full batch optimizers
+            self.run_stochastic(n_iter)
         else:
-
-            # Set up the problem
-
-            # chop's full batch optimizers require
-            # (batch_size, *shape) shape
-            x0 = x0.reshape(1, -1)
-            @chop.utils.closure
-            def logloss(x):
-
-                alpha = self.lmbd / X.size(0)
-                out = chop.utils.bmv(X, x)
-                loss = criterion(out, y)
-                reg = .5 * alpha * (x ** 2).sum()
-                return loss + reg
-
-            # Solve the problem
-            if solver == 'pgd':
-                if self.line_search:
-                    step = 'backtracking'
-                else:
-                    # estimate the step using backtracking line search once
-                    step = None
-
-                result = chop.optim.minimize_pgd(logloss, x0,
-                                                 prox=lambda x, s=None: x,
-                                                 step=step,
-                                                 max_iter=n_iter)
-
-            else:
-                raise NotImplementedError
-
-            self.beta = result.x
+            self.run_full_batch(n_iter)
 
     def get_result(self):
         return self.beta.detach().cpu().numpy().flatten()
