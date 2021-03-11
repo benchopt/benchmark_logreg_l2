e1ed838bb92846da61cd543bf60c391ed1dc2c2b
Geoffrey Négiar <geoffrey_negiar@berkeley.edu>
2021-03-11 00:24:13 -0800

    Added stochastic optimizer (SGD) + options

--- solvers/chop.py
+++ solvers/chop.py
@@ -1,9 +1,12 @@
 import warnings
 from benchopt import BaseSolver, safe_import_context
+from torch.utils.data.dataset import TensorDataset
 
 with safe_import_context() as import_ctx:
     import torch
+    from torch.utils.data import DataLoader
     import chop
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -14,58 +17,134 @@ class Solver(BaseSolver):
     install_cmd = 'conda'
     requirements = ['pip:https://github.com/openopt/chop/archive/master.zip']
 
-    parameters = {'solver': ['pgd'],
-                  'line_search': [False, True]}
+    parameters = {
+        'solver': ['pgd'],
+        'line_search': [False, True],
+        'stochastic': [False, True],
+        'batch_size': [32, 128, 200, 'full'],
+        'normalization': ['none', 'L2', 'Linf', 'sign'],
+        'momentum': [0., 0.9]
+        }
 
     def skip(self, X, y, lmbd):
+        if not self.stochastic and self.batch_size != 'full':
+            return True, "We only run stochastic=False if batch_size=='full'."
+
+        if self.stochastic:
+            if self.batch_size == 'full':
+                msg = "We do not perform full batch optimization "\
+                    "with a stochastic optimizer."
+                return True, msg
+
+            if self.line_search:
+                msg = "We do not perform line-search with a "\
+                    "stochastic optimizer."
+                return True, msg
+
+        else:
+            if self.normalization != 'none':
+                msg = 'Normalizations are not used for full batch '\
+                    'optimizers.'
+                return True, msg
+
+            if self.momentum != 0.:
+                msg = 'Momentum has no impact on full batch optimizers.'
+                return True, msg
+
         return False, None
 
     def set_objective(self, X, y, lmbd):
         self.lmbd = lmbd
 
-        self.X = torch.tensor(X)
-        self.y = torch.tensor(y > 0, dtype=torch.float64)
+        self.X = torch.tensor(X).to(device)
+        self.y = torch.tensor(y > 0, dtype=torch.float64).to(device)
 
         # Not sure if this is useful here
         self.run(1)
 
     def run(self, n_iter):
         X, y, solver = self.X, self.y, self.solver
-        # Decide whether to solve a batch of problems
+
         n_points, n_features = X.shape
 
-        x0 = torch.zeros(1, n_features, requires_grad=True, dtype=X.dtype)
+        x0 = torch.zeros(n_features, dtype=X.dtype, device=device)
         if n_iter == 0:
             return x0
 
         criterion = torch.nn.BCEWithLogitsLoss()
 
-        @chop.utils.closure
-        def logloss(x):
-
-            alpha = self.lmbd / X.size(0)
-            out = chop.utils.bmv(X, x)
-            loss = criterion(out, y)
-            reg = .5 * alpha * chop.utils.bdot(x, x).squeeze()
-            return loss + reg
-
         warnings.filterwarnings('ignore', category=RuntimeWarning)
 
-        if solver == 'pgd':
-            if self.line_search:
-                step = 'backtracking'
+        if self.stochastic:
+
+            # prepare dataset
+            dataset = TensorDataset(X, y)
+            loader = DataLoader(dataset, batch_size=self.batch_size)
+
+            # prepare opt variable
+            x = x0.clone().detach().flatten()
+            x.requires_grad_(True)
+
+            if solver == 'pgd':
+                optimizer = chop.stochastic.PGD([x], lr=.05, momentum=self.momentum,
+                                                normalization=self.normalization)
+
             else:
-                # estimate the step using backtracking line search once
-                step = None
+                raise NotImplementedError
 
-            result = chop.optim.minimize_pgd(logloss, x0,
-                                             prox=lambda x, s: x,
-                                             step=step,
-                                             max_iter=n_iter)
+            # Optimization loop
+            counter = 0
+
+            alpha = self.lmbd / X.size(0)
+
+            while counter < n_iter:
+
+                for data, target in loader:
+                    counter += 1
+
+                    optimizer.zero_grad()
+                    pred = data @ x
+                    loss = criterion(pred, target)
+                    loss += .5 * alpha * (x ** 2).sum()
+                    loss.backward()
+                    optimizer.step()
+
+            self.beta = x.detach().clone()
+
+        # Full batch optimizers
         else:
-            raise NotImplementedError
 
-        self.beta = result.x
+            # Set up the problem
+
+            # chop's full batch optimizers require
+            # (batch_size, *shape) shape
+            x0 = x0.reshape(1, -1)
+            @chop.utils.closure
+            def logloss(x):
+
+                alpha = self.lmbd / X.size(0)
+                out = chop.utils.bmv(X, x)
+                loss = criterion(out, y)
+                reg = .5 * alpha * (x ** 2).sum()
+                return loss + reg
+
+            # Solve the problem
+            if solver == 'pgd':
+                if self.line_search:
+                    step = 'backtracking'
+                else:
+                    # estimate the step using backtracking line search once
+                    step = None
+
+                result = chop.optim.minimize_pgd(logloss, x0,
+                                                 prox=lambda x, s=None: x,
+                                                 step=step,
+                                                 max_iter=n_iter)
+
+            else:
+                raise NotImplementedError
+
+            self.beta = result.x
 
     def get_result(self):
-        return self.beta.cpu().detach().numpy().flatten()
+        return self.beta.detach().cpu().numpy().flatten()
