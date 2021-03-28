778883e78bb03a8d92504aedbc3bd1b8042cc7fa
Geoffrey Négiar <geoffrey_negiar@berkeley.edu>
2021-03-28 01:15:05 -0700

    Add standardize parameter

--- datasets/covtype.py
+++ datasets/covtype.py
@@ -3,6 +3,7 @@ from benchopt import BaseDataset, safe_import_context
 
 with safe_import_context() as import_ctx:
     from sklearn.datasets import fetch_covtype
+    from sklearn.preprocessing import StandardScaler
 
 
 class Dataset(BaseDataset):
@@ -11,9 +12,18 @@ class Dataset(BaseDataset):
     install_cmd = 'conda'
     requirements = ['pip:scikit-learn']
 
+    parameters = {
+        'standardized': [False, True]
+    }
+
     def get_data(self):
         X, y = fetch_covtype(return_X_y=True)
         y[y != 2] = -1
         y[y == 2] = 1  # try to separate class 2 from the other 6 classes.
+
+        if self.standardized:
+            scaler = StandardScaler()
+            X = scaler.fit_transform(X)
         data = dict(X=X, y=y)
+
         return X.shape[1], data
