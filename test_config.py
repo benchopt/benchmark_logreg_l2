c31f992c1bbbcc7479070c78b80d75a418fb70af
tomMoral <thomas.moreau.2010@gmail.com>
2021-09-15 16:53:59 +0200

    FIX check_test name in test_config

--- test_config.py
+++ test_config.py
@@ -1,21 +1,13 @@
-import sys
+import sys  # noqa: F401
 
-import pytest
+import pytest  # noqa: F401
 
 
-def xfail_test_solver_install(solver_class):
+def check_test_solver_install(solver_class):
+    """Hook called in `test_solver_install`.
 
-    if solver_class.name.lower() == 'cyanure' and sys.platform == 'darwin':
-        pytest.xfail('Cyanure is not easy to install on macos.')
-
-    # Skip test_solver_install for julia in OSX as there is a version
-    # conflict with conda packages for R
-    # See issue benchopt/benchopt#64
-    if 'julia' in solver_class.name.lower() and sys.platform == 'darwin':
-        pytest.xfail('Julia causes segfault on OSX for now.')
-
-    # Lightning install is broken on python3.9+.
-    # See issue #XX
-    if (solver_class.name.lower() == 'lightning'
-            and sys.version_info >= (3, 9)):
-        pytest.xfail('Lightning install is broken on python3.9+.')
+    If one solver needs to be skip/xfailed on some
+    particular architecture, call pytest.xfail when
+    detecting the situation.
+    """
+    pass
