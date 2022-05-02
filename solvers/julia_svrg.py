9c8b630e1386b184a3ecddf3d9196467515c6c69
tbng <tuanbinhs@gmail.com>
2022-05-02 09:22:17 +0200

    added working-ish script that returns convergence curve

--- solvers/julia_svrg.py
+++ solvers/julia_svrg.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from benchopt import safe_import_context
 
 from benchopt.helpers.julia import JuliaSolver
@@ -9,12 +10,12 @@ with safe_import_context() as import_ctx:
 
 
 # File containing the function to be called from julia
-JULIA_SOLVER_FILE = 'julia_saga_batch.jl'
+JULIA_SOLVER_FILE = str(Path(__file__).with_suffix('.jl'))
 
 
 class Solver(JuliaSolver):
 
-    name = 'Julia-SAGA-Batch'
+    name = 'julia-svrg'
     references = [
         'Gazagnadou, Nidham, Robert Gower, and Joseph Salmon,'
         ' "Optimal mini-batch and step sizes for SAGA." '
@@ -22,28 +23,20 @@ class Solver(JuliaSolver):
         'https://github.com/gowerrobert/StochOpt.jl'
     ]
 
-    # List of dependencies can be found on the package github
-    julia_requirements = [
-        'StochOpt::https://github.com/tommoral/StochOpt.jl#FIX_proper_module_install',  # noqa: E501
-    ]
+    def set_requirements(self):
+        # List of dependencies can be found on the package github
+        self.julia_requirements = [
+            'https://github.com/tbng/StochOpt.jl',
+        ]
 
-    def skip(self, X, y, lmbd, fit_intercept):
-        # fit intercept is not yet implemented in StochOpt.jl
-        if fit_intercept:
-            return True, f"{self.name} does not handle fit_intercept"
+    def set_objective(self, X, y, lmbd):
 
-        return False, None
-
-    def set_objective(self, X, y, lmbd, fit_intercept):
         self.X, self.y, self.lmbd = X, y, lmbd
-        self.fit_intercept = fit_intercept
-
         jl = get_jl_interpreter()
         self.solve_logreg_l2 = jl.include(JULIA_SOLVER_FILE)
 
     def run(self, n_iter):
-        self.beta = self.solve_lasso(self.X, self.y, self.lmbd, n_iter)
+        self.beta = self.solve_logreg_l2(self.X.transpose(), self.y, self.lmbd, n_iter)[1:]
 
     def get_result(self):
         return self.beta.ravel()
-
