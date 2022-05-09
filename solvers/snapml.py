448b4dbc77543c7b04454a6290365953faabab67
Tanguy Lefort <tanguy_l@outlook.fr>
2022-05-09 20:42:14 +0200

    use gpu snapml

--- solvers/snapml.py
+++ solvers/snapml.py
@@ -1,4 +1,7 @@
 from benchopt import BaseSolver, safe_import_context
+from benchopt.utils.sys_info import _get_cuda_version
+
+cuda_version = _get_cuda_version()
 
 with safe_import_context() as import_ctx:
     from snapml import LogisticRegression
@@ -13,12 +16,14 @@ class Solver(BaseSolver):
 
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
+        self.use_gpu = cuda_version is not None
 
         self.clf = LogisticRegression(
             fit_intercept=False,
             regularizer=self.lmbd,
             penalty="l2",
             tol=1e-12,
+            use_gpu=self.use_gpu,
         )
 
     def run(self, n_iter):
