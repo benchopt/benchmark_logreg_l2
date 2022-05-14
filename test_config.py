71897805064dfc5871f77e67d17d83069e038e5c
Tanguy Lefort <tanguy_l@outlook.fr>
2022-05-14 19:04:02 +0200

    MTN use public cuda version (#34)

--- test_config.py
+++ test_config.py
@@ -1,7 +1,7 @@
 import sys  # noqa: F401
 import pytest  # noqa: F401
 
-from benchopt.utils.sys_info import _get_cuda_version
+from benchopt.utils.sys_info import get_cuda_version
 
 
 def check_test_solver_install(solver_class):
@@ -11,7 +11,7 @@ def check_test_solver_install(solver_class):
     particular architecture, call pytest.xfail when
     detecting the situation.
     """
-    cuda_version = _get_cuda_version()
+    cuda_version = get_cuda_version()
     if solver_class.name.lower() == "cuml":
         if sys.platform == "darwin":
             pytest.xfail("Cuml is not supported on MacOS.")
