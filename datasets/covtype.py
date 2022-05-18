FIX uniform scaling for all datasets

datasets/covtype.py
@@ -3,6 +3,7 @@ from benchopt import BaseDataset, safe_import_context
 
 with safe_import_context() as import_ctx:
     from sklearn.datasets import fetch_covtype
+    from sklearn.preprocessing import StandardScaler
 
 
 class Dataset(BaseDataset):
@@ -22,7 +23,7 @@ class Dataset(BaseDataset):
 
         if self.scaled:
             # column scaling
-            X -= X.mean(axis=0)
-            X /= X.std(axis=0)
+            scaler = StandardScaler(with_mean=False)
+            X = scaler.fit_transform(X)
 
         return dict(X=X, y=y)

datasets/madelon.py
@@ -3,13 +3,14 @@ from benchopt import BaseDataset, safe_import_context
 
 with safe_import_context() as import_ctx:
     from libsvmdata import fetch_libsvm
+    from sklearn.preprocessing import StandardScaler
 
 
 class Dataset(BaseDataset):
     name = "madelon"
 
     install_cmd = 'conda'
-    requirements = ['pip:libsvmdata']
+    requirements = ['libsvmdata', 'scikit-learn']
 
     parameters = {
         'scaled': [True, False]
@@ -21,11 +22,9 @@ class Dataset(BaseDataset):
 
         if self.scaled:
             # column scaling
-            mu, sigma = X.mean(axis=0), X.std(axis=0)
-            X -= mu
-            X /= sigma
-            X_test -= mu
-            X_test /= sigma
+            scaler = StandardScaler(with_mean=False)
+            X = scaler.fit_transform(X)
+            X_test = scaler.transform(X_test)
 
         data = dict(X=X, y=y, X_test=X_test, y_test=y_test)
         return data

datasets/news20.py
@@ -4,6 +4,7 @@ from benchopt import BaseDataset, safe_import_context
 with safe_import_context() as import_ctx:
     # Dependencies of download_libsvm are scikit-learn, download and tqdm
     from libsvmdata import fetch_libsvm
+    from sklearn.preprocessing import StandardScaler
 
 
 class Dataset(BaseDataset):
@@ -11,7 +12,7 @@ class Dataset(BaseDataset):
     is_sparse = True
 
     install_cmd = "conda"
-    requirements = ["pip:libsvmdata"]
+    requirements = ["pip:libsvmdata", "scikit-learn"]
 
     parameters = {
         'scaled': [True, False]
@@ -23,7 +24,7 @@ class Dataset(BaseDataset):
 
         if self.scaled:
             # column scaling
-            X -= X.mean(axis=0)
-            X /= X.std(axis=0)
+            scaler = StandardScaler(with_mean=False)
+            X = scaler.fit_transform(X)
 
         return dict(X=X, y=y)